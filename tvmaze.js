c81d819e3b4634b58286ea3bc98a96f386ce9590
Johnathan Booy <johnny_music@icloud.com>
2022-08-09 11:53:14 -0700

    Steps 1to4

--- tvmaze.js
+++ tvmaze.js
@@ -4,7 +4,6 @@ const $showsList = $("#shows-list");
 const $episodesArea = $("#episodes-area");
 const $searchForm = $("#search-form");
 
-
 /** Given a search term, search for tv shows that match that query.
  *
  *  Returns (promise) array of show objects: [show, show, ...].
@@ -12,41 +11,37 @@ const $searchForm = $("#search-form");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
-
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+function extractShowData(res) {
+	const shows = [];
+	for (const entry of res.data) {
+		const { id, name, summary } = entry.show;
+		const image = entry.show.image
+			? entry.show.image.medium
+			: "https://tinyurl.com/tv-missing";
+		const show = { id, name, summary, image };
+		shows.push(show);
+	}
+	return shows;
 }
 
+async function getShowsByTerm(term) {
+	const res = await axios.get("http://api.tvmaze.com/search/shows", {
+		params: { q: term },
+	});
+	return extractShowData(res);
+}
 
 /** Given list of shows, create markup for each and to DOM */
 
 function populateShows(shows) {
-  $showsList.empty();
+	$showsList.empty();
 
-  for (let show of shows) {
-    const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+	for (let show of shows) {
+		const $show = $(
+			`<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
+              src="${show.image}" 
               alt="Bletchly Circle San Francisco" 
               class="w-25 mr-3">
            <div class="media-body">
@@ -58,29 +53,29 @@ function populateShows(shows) {
            </div>
          </div>  
        </div>
-      `);
+      `
+		);
 
-    $showsList.append($show);  }
+		$showsList.append($show);
+	}
 }
-
 
 /** Handle search form submission: get shows from API and display.
  *    Hide episodes area (that only gets shown if they ask for episodes)
  */
 
 async function searchForShowAndDisplay() {
-  const term = $("#searchForm-term").val();
-  const shows = await getShowsByTerm(term);
+	const term = $("#search-query").val();
+	const shows = await getShowsByTerm(term);
 
-  $episodesArea.hide();
-  populateShows(shows);
+	$episodesArea.hide();
+	populateShows(shows);
 }
 
 $searchForm.on("submit", async function (evt) {
-  evt.preventDefault();
-  await searchForShowAndDisplay();
+	evt.preventDefault();
+	await searchForShowAndDisplay();
 });
-
 
 /** Given a show ID, get from API and return (promise) array of episodes:
  *      { id, name, season, number }
